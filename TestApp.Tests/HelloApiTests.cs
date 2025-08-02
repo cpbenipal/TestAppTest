aa83a22a46f44a01aeab8da427bd32d1a703b984
cpbenipal <cpbenipal@gmail.com>
2025-08-03 02:22:15 +0530

    adasd

--- TestApp.Tests/HelloApiTests.cs
+++ TestApp.Tests/HelloApiTests.cs
@@ -9,7 +9,7 @@ namespace TestApp.Tests
     public class HelloApiTests
     {
         private static readonly HttpClient client = new HttpClient();
-        private string _baseUrl = "http://localhost:8080";
+        private string _baseUrl = "http://<service-name>:8080";
        // private string _baseUrl = "http://host.docker.internal:5000";
         
         [Test]
